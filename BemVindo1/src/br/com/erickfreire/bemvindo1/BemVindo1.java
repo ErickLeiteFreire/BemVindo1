d59bb45c2d147ad9b4cc25e498e05b43706978cd
Erick Freire <erilefr@gmail.com>
2020-12-28 22:16:36 -0300

    Programa em Java que apresenta uma mensagem inicial simples
    Programa em Java que apresenta uma mensagem inicial simples

---
+++ BemVindo1/src/br/com/erickfreire/bemvindo1/BemVindo1.java
@@ -0,0 +1,15 @@
+package br.com.erickfreire.bemvindo1;
+
+/**
+ * Programa que exibe uma mensagem inicial simples em Java.
+ * @version 1 - Criado em 28-12-2020
+ * @author Erick Freire
+ *
+ */
+
+public class BemVindo1 {
+	public static void main(String[] args) {
+		System.out.println("Bem Vindo a Programação Em Java!");
+	}
+
+}
